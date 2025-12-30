6cccc8b2b18f84b8c2aad6bdd5283e78e5cb8642
Developer-Ronnie <ronnieworkspace@gmail.com>
2025-12-30 17:49:50 +0530

    initialize GTA-themed website with React, Vite, and Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="svg fixed top-0 left-0 z-[100] w-full h-screen overflow-hidden bg-[#000]"></div>
+  )
+}
+
+export default App
