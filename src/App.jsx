167defb7d7ba4be68c6c3e1e6b585d23b4b3302a
Developer-Ronnie <ronnieworkspace@gmail.com>
2026-01-02 17:52:59 +0530

    add SVG background with masked text for the main app component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,36 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className="svg fixed top-0 left-0 z-[100] w-full h-screen overflow-hidden bg-[#000]"></div>
+    <div className="svg fixed top-0 left-0 z-[100] w-full h-screen overflow-hidden bg-[#000]">
+      <svg viewBox="0 0 800 600" preserveAspectRatio='xMidYMid slice'>
+        <defs>
+          <mask id='viMask'>
+            <rect width='100%' height='100%' fill='black' />
+            <g className='vi-mask-group'>
+              <text
+                x='50%'
+                y='50%'
+                fontSize='250'
+                textAnchor='middle'
+                fill='white'
+                dominantBaseline='middle'
+                fontFamily='Arial Black'
+              >
+                VI
+              </text>
+            </g>
+          </mask>
+        </defs>
+        <image
+            href='https://unsplash.com/photos/black-suv-on-brown-grass-field-x7EkH3S6EjU'
+            width='100%'
+            height='100%'
+            preserveAspectRatio='xMidYMid slice'
+            mask='url(#viMask)'
+        />
+
+      </svg>
+    </div>
   )
 }
 
