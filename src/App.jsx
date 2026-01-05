4752dadc385b82e807898f21dfdcd15a756b5dfe
RONNIE <108121201+Developer-RONNIE@users.noreply.github.com>
2026-01-05 20:16:58 +0000

    add GSAP animations to App component and update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useGSAP } from '@gsap/react';
+import gsap from "gsap";
 
-const App = () => {
+function App()  {
+  let [showContent, setShowContent] = useState(false);
+  useGSAP(()=> {
+    const tl = gsap.timeline();
+
+    tl.to('.vi-mask-group', {
+      rotate: 10,
+      duration: 2,
+      ease: 'Power4.easeInOut',
+      transformOrigin: '50% 50%',
+    }).to('.vi-mask-group', {
+      scale: 10,
+      duration: 2,
+      delay: -1.8,
+      ease: 'expo.easeInOut',
+      transformOrigin: "50% 50%",
+      opacity: 0,
+      onUpdate: function(){
+        if(this.progress() >= .9){
+          document.querySelector('.svg').remove();
+          setShowContent(true);
+          this.kill();
+        }
+      }
+
+    });
+  });
   return (
-    <div className="svg fixed top-0 left-0 z-[100] w-full h-screen overflow-hidden bg-[#000]">
+    <div className="svg flex items-center justify-center fixed top-0 left-0 z-[100] w-full h-screen overflow-hidden bg-[#000]">
       <svg viewBox="0 0 800 600" preserveAspectRatio='xMidYMid slice'>
         <defs>
           <mask id='viMask'>
@@ -23,7 +51,7 @@ const App = () => {
           </mask>
         </defs>
         <image
-            href='https://unsplash.com/photos/black-suv-on-brown-grass-field-x7EkH3S6EjU'
+            href='/bg.png'
             width='100%'
             height='100%'
             preserveAspectRatio='xMidYMid slice'
